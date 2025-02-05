01bb2540ef1bb0b38f6e1f04c6c979a46e83c4ac
Tamirlan <tiimwqq@gmail.com>
2025-02-05 17:08:19 +0500

    feat: created top-bar, sort-popup, filter-checkbox components with styles -top-bar, sort-popup is top menu. top-bar contains sort-popup and categories, its a sticky component. -filter-checkbox is a component for future filters component

---
+++ components/shared/sort-popup.tsx
@@ -0,0 +1,12 @@
+import { ArrowUpDown } from 'lucide-react';
+import React from 'react';
+
+export const SortPopup: React.FC = () => {
+    return (
+        <div className='inline-flex items-center gap-2 bg-gray-50 px-5 h-[52px] rounded-2xl cursor-pointer'>
+            <ArrowUpDown size={16}/>
+            <span>сортировка: </span>
+            <b>популярное</b>
+        </div>
+    );
+};
---
+++ components/shared/top-bar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Categories } from './categories';
+import { SortPopup } from './sort-popup';
+import { Container } from './container';
+
+export const TopBar: React.FC = () => {
+    return (
+        <div className="sticky top-0 bg-white py-5 shadow-lg shadow-black/5 z-10">
+            <Container className="flex items-center justify-between ">
+                <Categories />
+                <SortPopup />
+            </Container>
+        </div>
+
+    );
+};
